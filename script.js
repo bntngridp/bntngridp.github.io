58f28251e6e4c081bfcb88dd332bc38f4c856009
Bintang Ridwan Pribadi <bintangridwan30@gmail.com>
2026-03-31 12:14:13 +0700

    Add theme toggle and personalize site content

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", function() {
-    header.classList.toggle("sticky",this.window.scrollY > 120);
-})
+    header.classList.toggle("sticky", this.window.scrollY > 120);
+});
 let menu = document.querySelector("#menu-icon");
 let navlist = document.querySelector('.navlist');
 
-menu.onclick = () =>{
+menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('active');
-}
+};
 
-window.onscroll = () =>{
+window.onscroll = () => {
     menu.classList.remove('bx-x');
     navlist.classList.remove('active');
-}
+};
 
 const toggle = document.getElementById("theme-toggle");
 
-toggle.onclick = () => {
-    document.body.classList.toggle("light-mode");
+if (toggle) {
+    const icon = toggle.querySelector("i");
+
+    toggle.addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+
+        if (document.body.classList.contains("light-mode")) {
+            icon.classList.remove("ri-moon-line");
+            icon.classList.add("ri-sun-line");
+        } else {
+            icon.classList.remove("ri-sun-line");
+            icon.classList.add("ri-moon-line");
+        }
+    });
 }
